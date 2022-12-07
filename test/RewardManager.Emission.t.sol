99d2f3280eaad39fec63e39326902ea212f8d37f
dejx <dejan@ld.si>
2022-12-07 16:37:30 +0100

    Fixed tests.

--- test/RewardManager.Emission.t.sol
+++ test/RewardManager.Emission.t.sol
@@ -97,7 +97,6 @@ contract RewardManagerEmissionTests is RewardManagerTests {
         rewardToken2.approve(address(rewardManager), rewardAmount / 2);
         rewardManager.addToken(smartVault, rewardToken2, rewardDuration, rewardAmount /2);
         vm.stopPrank();
-        console.log(rewardManager.rewardTokensCount(smartVault));
 
         rewardManager.updateRewardsOnVault(smartVault, user);
         deal(address(smartVault), user, data.userDeposit, true); // Depositing into a vault.
@@ -148,43 +147,67 @@ contract RewardManagerEmissionTests is RewardManagerTests {
         rewardToken2.approve(address(rewardManager), rewardAmount / 10);
         rewardManager.addToken(smartVault, rewardToken2, rewardDuration / 10, rewardAmount /10);
         vm.stopPrank();
-        assertEq(rewardManager.rewardTokensCount(smartVault), 2);
 
 
         rewardManager.updateRewardsOnVault(smartVault, user);
         deal(address(smartVault), user, rewardAmount, true); // Depositing into a vault.
         skip(rewardDuration  / 2);
         rewardManager.getActiveRewards(smartVault, user);
-        assertEq(rewardManager.rewardTokensCount(smartVault), 1);
 
-        console.log(rewardToken2.balanceOf(user));
+
+        assertEq(rewardToken2.balanceOf(user), 9999999999999999900000); // R2 claimed completely.
+        assertEq(rewardToken.balanceOf(user), 49999999999999999900000); // R1 claimed 50%
+        skip(rewardDuration / 2 + 1);
+        rewardManager.getActiveRewards(smartVault, user);
+        assertEq(rewardToken2.balanceOf(user), 9999999999999999900000); // R2 claimed completely before and stayed the same value.
+        assertEq(rewardToken.balanceOf(user), 99999999999999999800000); // R1 claimed 50%
         // user should get all rewards from R2 and half from R1 todo assert
     }
 
     function test_getActiveRewards_RemoveTokensAfterFinish() public {
         addRewardTokens();
         MockToken rewardToken2 = new MockToken("R2", "R2");
-        deal(address(rewardToken2), vaultOwner, rewardAmount, true); // Dealing a tenth of the R1 reward.
-
+        deal(address(rewardToken2), vaultOwner, rewardAmount/2, true); // Dealing half of the R1 reward.
 
         vm.startPrank(vaultOwner);
-        rewardToken2.approve(address(rewardManager), rewardAmount );
-        rewardManager.addToken(smartVault, rewardToken2, rewardDuration*2, rewardAmount);
+        rewardToken2.approve(address(rewardManager), rewardAmount/2 );
+        rewardManager.addToken(smartVault, rewardToken2, rewardDuration*2, rewardAmount/2);
         vm.stopPrank();
+
+        uint256 rewardCountBefore = rewardManager.rewardTokensCount(smartVault);
 
         rewardManager.updateRewardsOnVault(smartVault, user);
         deal(address(smartVault), user, rewardAmount, true); // Depositing into a vault.
 
-        skip(rewardDuration * 2);
+        uint256 user1balance1Before = rewardToken.balanceOf(user);
+        uint256 user1balance2Before = rewardToken2.balanceOf(user);
+
+        skip(rewardDuration * 10);
 
         vm.startPrank(vaultOwner);
         rewardManager.removeReward(smartVault, rewardToken2);
+        rewardManager.getActiveRewards(smartVault, user);
         vm.stopPrank();
 
-        rewardManager.getActiveRewards(smartVault, user);
         IERC20[] memory tokens = new IERC20 [] (1);
         tokens[0] = rewardToken2;
+
+        vm.prank(user);
         rewardManager.getRewards(smartVault, tokens);
+
+        uint256 rewardCountAfter = rewardManager.rewardTokensCount(smartVault);
+
+        assertEq(rewardCountBefore, 2);
+        assertEq(rewardCountAfter, 1);
+
+        uint256 user1balance1After = rewardToken.balanceOf(user);
+        uint256 user1balance2After = rewardToken2.balanceOf(user);
+        uint256 user1balance1Gain = user1balance1After - user1balance1Before;
+        uint256 user1balance2Gain = user1balance2After - user1balance2Before;
+
+
+        assertEq(user1balance1Gain, 99999999999999999900000);
+        assertEq(user1balance2Gain, 49999999999999999900000);
     }
     struct AssertData {
         uint256 userDeposit;
