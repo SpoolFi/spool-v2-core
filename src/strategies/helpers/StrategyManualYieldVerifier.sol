09b50375862e5a7872d0d420417f52b5a9beb446
ryptoc <dannoc1996@gmail.com>
2023-04-13 12:24:55 +0200

    Updated comments and error names

--- src/strategies/helpers/StrategyManualYieldVerifier.sol
+++ src/strategies/helpers/StrategyManualYieldVerifier.sol
@@ -4,19 +4,21 @@ pragma solidity 0.8.17;
 import "../../access/SpoolAccessControllable.sol";
 
 /// @notice Used when manual yield is more than allowed.
-error TooBigManualYield(int256 manualYield);
+error ManualYieldTooBig(int256 manualYield);
 
 /// @notice Used when manual yield is less than allowed.
-error TooSmallManualYield(int256 manualYield);
+error ManualYieldTooSmall(int256 manualYield);
 
 abstract contract StrategyManualYieldVerifier is SpoolAccessControllable {
-    /// @notice
+    /// @notice Positive manual yield limit.
     int128 public positiveLimit;
+
+    /// @notice Negative manual yield limit.
     int128 public negativeLimit;
 
     /**
      * @notice Sets positive yield limit.
-     * @param positiveLimit_ new positive yield limit.
+     * @param positiveLimit_ New positive yield limit.
      */
     function setPositiveLimit(int128 positiveLimit_) external onlyRole(ROLE_SPOOL_ADMIN, msg.sender) {
         _setPositiveLimit(positiveLimit_);
@@ -24,7 +26,7 @@ abstract contract StrategyManualYieldVerifier is SpoolAccessControllable {
 
     /**
      * @notice Sets negative yield limit.
-     * @param negativeLimit_ new negative yield limit.
+     * @param negativeLimit_ New negative yield limit.
      */
     function setNegativeLimit(int128 negativeLimit_) external onlyRole(ROLE_SPOOL_ADMIN, msg.sender) {
         _setNegativeLimit(negativeLimit_);
@@ -47,11 +49,11 @@ abstract contract StrategyManualYieldVerifier is SpoolAccessControllable {
     function _verifyManualYieldPercentage(int256 manualYield) internal view virtual {
         if (manualYield > 0) {
             if (manualYield > positiveLimit) {
-                revert TooBigManualYield(manualYield);
+                revert ManualYieldTooBig(manualYield);
             }
         } else if (manualYield < 0) {
             if (manualYield < negativeLimit) {
-                revert TooSmallManualYield(manualYield);
+                revert ManualYieldTooSmall(manualYield);
             }
         }
     }
