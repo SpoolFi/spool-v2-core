e17ec6493ec2fb3a563417bdf1231926e4a8f22e
Tadhg Riordan <riordant@tcd.ie>
2024-02-01 14:45:30 +0000

    Implementation with slippages

---
+++ src/external/interfaces/strategies/gearbox/v3/IFarmingPool.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {IERC20} from "@openzeppelin/token/ERC20/IERC20.sol";
+
+interface IFarmingPool is IERC20 {
+    event DistributorChanged(address oldDistributor, address newDistributor);
+    event RewardUpdated(uint256 reward, uint256 duration);
+
+    // View functions
+    function distributor() external view returns (address);
+    function stakingToken() external view returns (address);
+    function rewardsToken() external view returns (address);
+    function farmed(address account) external view returns (uint256);
+
+    // User functions
+    function deposit(uint256 amount) external;
+    function withdraw(uint256 amount) external;
+    function claim() external;
+    function exit() external;
+
+    // Owner functions
+    function setDistributor(address distributor_) external;
+
+    // Distributor functions
+    function startFarming(uint256 amount, uint256 period) external;
+    function rescueFunds(IERC20 token, uint256 amount) external;
+}
--- src/strategies/GearboxV3Strategy.sol
+++ src/strategies/GearboxV3Strategy.sol
@@ -4,79 +4,110 @@ pragma solidity 0.8.17;
 import "@openzeppelin/token/ERC20/IERC20.sol";
 import "@openzeppelin/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/utils/math/Math.sol";
-import "../external/interfaces/strategies/compound/v2/IComptroller.sol";
-import "../external/interfaces/strategies/compound/v2/ICErc20.sol";
+import "../external/interfaces/strategies/gearbox/v3/IFarmingPool.sol";
+import "../external/interfaces/strategies/gearbox/v3/IPoolV3.sol";
 import "../interfaces/ISwapper.sol";
+import "../libraries/PackedRange.sol";
 import "../strategies/Strategy.sol";
 
-/// @notice Used when Gearbox V2 mint returns an error code.
-error BadGearboxV3Deposit();
+error GearboxV3DepositCheckFailed();
+error GearboxV3RedeemalCheckFailed();
+error GearboxV3DepositSlippagesFailed();
+error GearboxV3RedeemalSlippagesFailed();
+error GearboxV3DepositFailed();
+error GearboxV3RedeemalFailed();
 
-/// @notice Used when Gearbox V2 redeem returns an error code.
-error BadGearboxV3Withdrawal();
+// One asset: WETH || USDC
+//
+// One reward: GEAR
+//
+// slippages:
+// - mode selection: slippages[0]
+//
+// - DHW with deposit: slippages[0] == 0
+//   - beforeDepositCheck: slippages[1]
+//   - beforeRedeemalCheck: slippages[2]
+//   - compound: slippages[3]
+//   - _depositToProtocol: slippages[4]
+//
+// - DHW with withdrawal: slippages[0] == 1
+//   - beforeDepositCheck: slippages[1]
+//   - beforeRedeemalCheck: slippages[2]
+//   - compound: slippages[3]
+//   - _redeemFromProtocol: slippages[4]
+//
+// - reallocate: slippages[0] == 2
+//   - beforeDepositCheck: depositSlippages[1]
+//   - _depositToProtocol: depositSlippages[2]
+//   - beforeRedeemalCheck: withdrawalSlippages[1]
+//   - _redeemFromProtocol: withdrawalSlippages[2]
 
+// - redeemFast or emergencyWithdraw: slippages[0] == 3
+//   - _redeemFromProtocol: slippages[1]
+//   - _emergencyWithdrawImpl: slippages[1]
+//
+// Description:
+// This is a Convex strategy. ETH is swapped for stETH (Lido) and frxETH,
+// and used to add proportional liquidity to the Curve st-frxETH Factory
+// Plain Pool, with the outgoing LP token being staked on Convex for boosted
+// rewards.
+//
+// The strategy supports two ways to obtain each of the stETH and frxETH tokens:
+// either by acquiring it directly on their respective protocols, or by buying
+// the token via their respective Curve ETH pools (See the adapter libraries
+// for each token).
+//
+// The do-hard-worker can decide which way to use based on profitability by
+// setting appropriate slippages (see slippages above).
+//
+// Since staked ETH on Lido and Frax is used to spin-up validators, it cannot
+// be unstaked immediately. To exit the protocol, the strategy sells the
+// tokens on their respective Curve ETH pools.
 contract GearboxV3Strategy is Strategy {
     using SafeERC20 for IERC20;
 
-    uint256 public immutable MANTISSA = 10 ** 18;
-
-    /// @notice Comptroller implementation
+    /// @notice Swapper implementation
     ISwapper public immutable swapper;
 
-    /// @notice Comptroller implementation
-    IComptroller public immutable comptroller;
-
     /// @notice COMP token
-    /// @dev Reward token when participating in the Gearbox protocol.
-    IERC20 public immutable comp;
+    /// @dev Reward token when participating in the GearboxV3 protocol.
+    IERC20 public immutable gear;
 
-    /// @notice Gearbox market
-    ICErc20 public cToken;
+    /// @notice dToken implementation (staking token)
+    IPoolV3 public immutable dToken;
 
-    /// @notice exchangeRateCurrent at the last DHW.
-    uint256 private _lastExchangeRate;
+    /// @notice sdToken implementation (lp token)
+    IFarmingPool public immutable sdToken;
+
+    /// @notice supplyRate at the last DHW.
+    uint256 private _lastSupplyRate;
 
     constructor(
         IAssetGroupRegistry assetGroupRegistry_,
         ISpoolAccessControl accessControl_,
         ISwapper swapper_,
-        IComptroller comptroller_
+        IFarmingPool sdToken_
     ) Strategy(assetGroupRegistry_, accessControl_, NULL_ASSET_GROUP_ID) {
         if (address(swapper_) == address(0)) revert ConfigurationAddressZero();
-        if (address(comptroller_) == address(0)) revert ConfigurationAddressZero();
-
-        if (!comptroller_.isComptroller()) {
-            revert InvalidConfiguration();
-        }
+        if (address(sdToken_) == address(0)) revert ConfigurationAddressZero();
 
         swapper = swapper_;
-        comptroller = comptroller_;
-        comp = IERC20(comptroller_.getCompAddress());
+        sdToken = sdToken_;
+
+        dToken = IPoolV3(sdToken_.stakingToken());
+        gear = IERC20(sdToken_.rewardsToken());
     }
 
-    function initialize(string memory strategyName_, uint256 assetGroupId_, ICErc20 cToken_) external initializer {
+    function initialize(string memory strategyName_, uint256 assetGroupId_) external initializer {
         __Strategy_init(strategyName_, assetGroupId_);
-
-        if (address(cToken_) == address(0)) {
-            revert ConfigurationAddressZero();
-        }
 
         address[] memory tokens = assets();
 
-        if (tokens.length != 1 || tokens[0] != cToken_.underlying()) {
+        if (tokens.length != 1 || tokens[0] != dToken.underlyingToken()) {
             revert InvalidAssetGroup(assetGroupId());
         }
 
-        address[] memory markets = new address[](1);
-        markets[0] = address(cToken_);
-        uint256[] memory results = comptroller.enterMarkets(markets);
-
-        if (results[0] > 0) {
-            revert InvalidConfiguration();
-        }
-
-        cToken = cToken_;
-        _lastExchangeRate = cToken_.exchangeRateCurrent();
+        _lastSupplyRate = dToken.supplyRate();
     }
 
     function assetRatio() external pure override returns (uint256[] memory) {
@@ -87,7 +118,7 @@ contract GearboxV3Strategy is Strategy {
 
     function getUnderlyingAssetAmounts() external view returns (uint256[] memory amounts) {
         amounts = new uint256[](1);
-        amounts[0] = (cToken.exchangeRateStored() * cToken.balanceOf(address(this))) / MANTISSA;
+        amounts[0] = _getdTokenValue(sdToken.balanceOf(address(this)));
     }
 
     /**
@@ -95,93 +126,100 @@ contract GearboxV3Strategy is Strategy {
      */
     function _swapAssets(address[] memory, uint256[] memory, SwapInfo[] calldata) internal override {}
 
-    function _compound(address[] calldata tokens, SwapInfo[] calldata swapInfo, uint256[] calldata)
+    function _compound(address[] calldata tokens, SwapInfo[] calldata swapInfo, uint256[] calldata slippages)
         internal
         override
         returns (int256 compoundedYieldPercentage)
     {
         if (swapInfo.length > 0) {
-            uint256 compBalance = _getGearboxReward();
+            uint256 gearBalance = _getGearboxReward();
 
-            if (compBalance > 0) {
-                comp.safeTransfer(address(swapper), compBalance);
+            if (gearBalance > 0) {
+                gear.safeTransfer(address(swapper), gearBalance);
                 address[] memory tokensIn = new address[](1);
-                tokensIn[0] = address(comp);
+                tokensIn[0] = address(gear);
                 uint256 swappedAmount = swapper.swap(tokensIn, swapInfo, tokens, address(this))[0];
 
                 if (swappedAmount > 0) {
-                    uint256 cTokenBalanceBefore = cToken.balanceOf(address(this));
-                    _depositToGearboxProtocol(IERC20(tokens[0]), swappedAmount);
+                    uint256 sdTokenBalanceBefore = sdToken.balanceOf(address(this));
+                    _depositToProtocolInternal(IERC20(tokens[0]), swappedAmount, slippages[1]);
 
                     compoundedYieldPercentage =
-                        _calculateYieldPercentage(cTokenBalanceBefore, cToken.balanceOf(address(this)));
+                        _calculateYieldPercentage(sdTokenBalanceBefore, sdToken.balanceOf(address(this)));
                 }
             }
         }
     }
 
     function _getYieldPercentage(int256) internal override returns (int256 baseYieldPercentage) {
-        uint256 exchangeRateCurrent = cToken.exchangeRateCurrent();
+        uint256 supplyRateCurrent = dToken.supplyRate();
 
-        baseYieldPercentage = _calculateYieldPercentage(_lastExchangeRate, exchangeRateCurrent);
-        _lastExchangeRate = exchangeRateCurrent;
+        baseYieldPercentage = _calculateYieldPercentage(_lastSupplyRate, supplyRateCurrent);
+        _lastSupplyRate = supplyRateCurrent;
     }
 
-    function _depositToProtocol(address[] calldata tokens, uint256[] memory amounts, uint256[] calldata)
+    function _depositToProtocol(address[] calldata tokens, uint256[] memory amounts, uint256[] calldata slippages)
         internal
         override
     {
-        _depositToGearboxProtocol(IERC20(tokens[0]), amounts[0]);
-    }
-
-    function _depositToGearboxProtocol(IERC20 token, uint256 amount) private {
-        if (amount > 0) {
-            _resetAndApprove(token, address(cToken), amount);
-
-            if (cToken.mint(amount) > 0) {
-                revert BadGearboxV3Deposit();
-            }
+        uint256 slippage;
+        if (slippages[0] == 0) {
+            slippage = slippages[4];
+        } else if (slippages[0] == 2) {
+            slippage = slippages[2];
+        } else {
+            revert GearboxV3DepositSlippagesFailed();
         }
+        _depositToProtocolInternal(IERC20(tokens[0]), amounts[0], slippage);
     }
 
     /**
-     * @notice Withdraw lp tokens from the Gearbox market
+     * @notice Withdraw lp tokens from the GearboxV3 market
      */
-    function _redeemFromProtocol(address[] calldata, uint256 ssts, uint256[] calldata) internal override {
+    function _redeemFromProtocol(address[] calldata, uint256 ssts, uint256[] calldata slippages) internal override {
+        uint256 slippage;
+        if (slippages[0] == 1) {
+            slippage = slippages[4];
+        } else if (slippages[0] == 2) {
+            slippage = slippages[2];
+        } else if (slippages[0] == 3) {
+            slippage = slippages[1];
+        } else if (slippages[0] == 0 && _isViewExecution()) {
+            slippage = slippages[4];
+        } else {
+            revert GearboxV3RedeemalSlippagesFailed();
+        }
+
         if (ssts == 0) {
             return;
         }
 
-        uint256 cTokenWithdrawAmount = (cToken.balanceOf(address(this)) * ssts) / totalSupply();
+        uint256 dTokenWithdrawAmount = (sdToken.balanceOf(address(this)) * ssts) / totalSupply();
 
-        if (cTokenWithdrawAmount > 0) {
-            if (cToken.redeem(cTokenWithdrawAmount) > 0) {
-                revert BadGearboxV3Withdrawal();
-            }
-        }
+        _redeemFromProtocolInternal(dTokenWithdrawAmount, slippage);
     }
 
-    function _emergencyWithdrawImpl(uint256[] calldata, address recipient) internal override {
-        uint256 cTokenBalance = cToken.balanceOf(address(this));
-
-        if (cTokenBalance > 0) {
-            if (cToken.redeem(cTokenBalance) > 0) {
-                revert BadGearboxV3Withdrawal();
-            }
-
-            address[] memory tokens = assets();
-            IERC20(tokens[0]).safeTransfer(recipient, IERC20(tokens[0]).balanceOf(address(this)));
+    function _emergencyWithdrawImpl(uint256[] calldata slippages, address recipient) internal override {
+        if (slippages[0] != 3) {
+            revert GearboxV3RedeemalSlippagesFailed();
         }
+
+        uint256 sdTokenBalance = sdToken.balanceOf(address(this));
+
+        _redeemFromProtocolInternal(sdTokenBalance, slippages[1]);
+        address[] memory tokens = assets();
+        IERC20(tokens[0]).safeTransfer(recipient, IERC20(tokens[0]).balanceOf(address(this)));
     }
 
     function _getUsdWorth(uint256[] memory exchangeRates, IUsdPriceFeedManager priceFeedManager)
         internal
+        view
         override
         returns (uint256 usdValue)
     {
-        uint256 cTokenBalance = cToken.balanceOf(address(this));
-        if (cTokenBalance > 0) {
-            uint256 tokenValue = _getcTokenValue(cTokenBalance);
+        uint256 sdTokenBalance = sdToken.balanceOf(address(this));
+        if (sdTokenBalance > 0) {
+            uint256 tokenValue = _getdTokenValue(sdTokenBalance);
 
             address[] memory assetGroup = _assetGroupRegistry.listAssetGroup(assetGroupId());
             usdValue = priceFeedManager.assetToUsdCustomPrice(assetGroup[0], tokenValue, exchangeRates[0]);
@@ -189,37 +227,96 @@ contract GearboxV3Strategy is Strategy {
     }
 
     /**
-     * @dev Get value of the desire cTkoen amount in the asset token amount
-     * @param cTokenAmount cToken amount
-     * @return tokenAmount value of `cTokenAmount` in asset tokens
+     * @dev Get value of the desired dToken amount in the asset token amount
+     * @param dTokenAmount dToken amount
+     * @return tokenAmount value of `dTokenAmount` in asset tokens
      */
-    function _getcTokenValue(uint256 cTokenAmount) private returns (uint256) {
-        if (cTokenAmount == 0) {
+    function _getdTokenValue(uint256 dTokenAmount) private view returns (uint256) {
+        if (dTokenAmount == 0) {
             return 0;
         }
 
-        return (cToken.exchangeRateCurrent() * cTokenAmount) / MANTISSA;
+        return dToken.previewRedeem(dTokenAmount);
     }
 
-    function beforeDepositCheck(uint256[] memory, uint256[] calldata) public view override {}
+    function beforeDepositCheck(uint256[] memory amounts, uint256[] calldata slippages) public override {
+        unchecked {
+            if (_isViewExecution()) {
+                uint256[] memory beforeDepositCheckSlippageAmounts = new uint256[](1);
+                beforeDepositCheckSlippageAmounts[0] = amounts[0];
+                emit BeforeDepositCheckSlippages(beforeDepositCheckSlippageAmounts);
+                return;
+            }
 
-    function beforeRedeemalCheck(uint256, uint256[] calldata) public view override {}
+            if (slippages[0] > 2) {
+                revert GearboxV3DepositCheckFailed();
+            }
+
+            if (!PackedRange.isWithinRange(slippages[1], amounts[0])) {
+                revert GearboxV3DepositCheckFailed();
+            }
+        }
+    }
+
+    function beforeRedeemalCheck(uint256 ssts, uint256[] calldata slippages) public override {
+        if (_isViewExecution()) {
+            emit BeforeRedeemalCheckSlippages(ssts);
+            return;
+        }
+
+        uint256 slippage;
+        if (slippages[0] < 2) {
+            slippage = slippages[2];
+        } else if (slippages[0] == 2) {
+            slippage = slippages[1];
+        } else {
+            revert GearboxV3RedeemalCheckFailed();
+        }
+
+        if (!PackedRange.isWithinRange(slippage, ssts)) {
+            revert GearboxV3RedeemalCheckFailed();
+        }
+    }
+
+    function _depositToProtocolInternal(IERC20 token, uint256 amount, uint256 slippage) internal {
+        if (amount > 0) {
+            _resetAndApprove(token, address(dToken), amount);
+
+            uint256 shares = dToken.deposit(amount, address(this));
+
+            if (shares < slippage) {
+                revert GearboxV3DepositFailed();
+            }
+
+            _resetAndApprove(dToken, address(sdToken), shares);
+            sdToken.deposit(shares);
+        }
+    }
+
+    function _redeemFromProtocolInternal(uint256 amount, uint256 slippage) internal {
+        if (amount > 0) {
+            sdToken.withdraw(amount);
+            uint256 shares = dToken.withdraw(amount, address(this), address(this));
+
+            if (shares < slippage) {
+                revert GearboxV3RedeemalFailed();
+            }
+        }
+    }
 
     function _getProtocolRewardsInternal() internal virtual override returns (address[] memory, uint256[] memory) {
         address[] memory tokens = new address[](1);
         uint256[] memory amounts = new uint256[](1);
 
-        tokens[0] = address(comp);
+        tokens[0] = address(gear);
         amounts[0] = _getGearboxReward();
 
         return (tokens, amounts);
     }
 
     function _getGearboxReward() internal returns (uint256) {
-        address[] memory markets = new address[](1);
-        markets[0] = address(cToken);
-        comptroller.claimComp(address(this), markets);
+        sdToken.claim();
 
-        return comp.balanceOf(address(this));
+        return gear.balanceOf(address(this));
     }
 }
