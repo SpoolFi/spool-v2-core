f01204ca01417dc7340abd082d42d7ceb960f469
ksamarin90 <ksamarin90@gmail.com>
2024-05-20 21:16:46 +0400

    fix rebase

--- script/mainnet/extended/YearnV3StrategyWithGaugeSetup.s.sol
+++ script/mainnet/extended/YearnV3StrategyWithGaugeSetup.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.17;
 
-import "../../MainnetExtendedSetup.s.sol";
+import "../../mainnet/MainnetExtendedSetup.s.sol";
 
 contract YearnV3WithGaugeStrategySetup is MainnetExtendedSetup {
     function execute() public override {
