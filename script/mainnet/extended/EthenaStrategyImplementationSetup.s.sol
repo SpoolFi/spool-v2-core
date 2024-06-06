f9e22455ad6017e126d48116b38976684c40b715
ksamarin90 <ksamarin90@gmail.com>
2024-06-06 11:22:02 +0400

    implementation deploy

---
+++ script/mainnet/extended/EthenaStrategyImplementationSetup.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.17;
+
+import "../MainnetExtendedSetup.s.sol";
+
+contract EthenaStrategyImplementationSetup is MainnetExtendedSetup {
+    function execute() public override {
+        StandardContracts memory contracts = StandardContracts({
+            accessControl: spoolAccessControl,
+            assetGroupRegistry: assetGroupRegistry,
+            swapper: swapper,
+            proxyAdmin: address(proxyAdmin),
+            strategyRegistry: strategyRegistry
+        });
+
+        // reserialize strategies
+        contractsJson().reserializeKeyAddress("strategies");
+
+        deployEthenaImpl(contracts);
+    }
+}
